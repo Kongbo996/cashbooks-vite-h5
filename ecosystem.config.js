05db7b04fb0d57c6c8281a27da086a706dfc3954
kongbo <kongbo5582@163.com>
2021-12-13 13:59:04 +0800

    fix: update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'cashbook-vite-h5',
-      script: 'cashbook-vite-h5-server.js'
+      script: 'vite-cashbook-port-server.js'
     },
   ],
   deploy: {
@@ -10,9 +10,9 @@ module.exports = {
       key: '~/.ssh/id_rsa',
       user: 'root',
       host: '81.68.165.190',
-      ref: 'origin/main',
-      repo: 'git@github.com:Kongbo996/cashbook-serve.git',
-      path: '/root/cashbook-serve/fe',
+      ref: 'origin/master',
+      repo: 'git@github.com:Kongbo996/cashbooks-vite-h5.git',
+      path: '/root/cashbook-vite-h5',
       'post-deploy': 'git reset --hard && git checkout main && git pull && npm i --production=false && npm run build && pm2 startOrReload ecosystem.config.js',
       env: {
         NODE_ENV: 'production'
